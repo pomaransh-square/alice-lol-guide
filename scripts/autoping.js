606e813d16e6d0621c4b89950d5fffd351a7a2a6
hyrdbyrd <ilya777grin23@yandex-team.ru>
2020-02-08 01:46:21 +0300

    delete not needs condition

--- scripts/autoping.js
+++ scripts/autoping.js
@@ -3,11 +3,6 @@ const path = require('path');
 
 const pingHost = process.env.HOST || 'https://alice-lol-guide.herokuapp.com/';
 
-if (!pingHost) {
-    console.log('env HOST not found');
-    process.exit(0);
-}
-
 // Every 10 minutes ping some host
 setInterval(() => {
     try { request.get(path.join(pingHost, 'ping')); } catch (e) {}
